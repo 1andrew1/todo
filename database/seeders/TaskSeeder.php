b00ea1ca80e34f38f348e456b7eb52a4304923cb
HPROBOOK\annak\Projekty <1andrew1@wp.pl>
2025-01-24 16:30:49 +0100

    add seeder

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Task;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Tworzenie 100 losowych zadań
+        for ($i = 0; $i < 100; $i++) {
+            Task::create([
+                'title' => 'Zadanie ' . ($i + 1), //Str::random(10),
+                'is_completed' => fake()->boolean(30), // 30% szans na zakończone zadanie
+            ]);
+        }
+    }
+}
